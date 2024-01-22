570d9e1281f0d2f9761507f377e7a234a3dc6b79
unknown <towork.matheusb@gmail.com>
2024-01-22 12:48:18 -0300

    :sparkles: include color picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import DownloadOutlined from '@ant-design/icons/lib/icons/DownloadOutlined';
-import { Button, Form, Input } from 'antd'
+import { Button, Form, Input, ColorPicker } from 'antd'
 import QrCode from 'antd/es/qr-code';
 import { useEffect, useState } from 'react'
 import { downloadSvg } from './services/download-svg';
@@ -14,13 +14,16 @@ function App() {
 
   const [typeQRCode, setTypeQRCode] = useState('svg')
 
+  const [color, setColor] = useState('#000')
+
   const onFinish = (values: any) => {
     setUrl(values.url)
   }
 
   useEffect(() => {
-    //console.log(url)
-  }, [url])
+    console.log(url)
+    console.log(color)
+  }, [url, color])
 
   return (
     <>
@@ -39,20 +42,29 @@ function App() {
                 name='url'>
                 <Input placeholder="google.com" />
               </Form.Item>
-              <Button type="primary" className='float-right bg-black' htmlType="submit">
-                Gerar QRCode
-              </Button>
+              <div className='flex justify-end gap-3'>
+                <Form.Item
+                  name='color'>
+                  <ColorPicker defaultValue="#000" showText disabledAlpha 
+                  onChange={(value, hex) => {
+                    setColor(hex)
+                  }}/>
+                </Form.Item>
+                <Button type="primary" className='float-right bg-black' htmlType="submit">
+                  Gerar QRCode
+                </Button>
+              </div>
             </Form>
           </div>
 
           <div className='flex flex-col justify-center gap-2 items-center p-8 rounded-xl h-auto w-auto max-sm:w-[80%] max-sm:items-start'>
             <div id="myqrcode" className='p-2'>
-              {typeQRCode === 'svg' && <QrCode type='svg' size={150} value={url} bgColor='#fff' />}
-              {typeQRCode === 'png' && <QrCode size={150} value={url} bgColor='#fff' />}
+              {typeQRCode === 'svg' && <QrCode color={color} type='svg' size={150} value={url} bgColor='transparent' />}
+              {typeQRCode === 'png' && <QrCode color={color} size={150} value={url} bgColor='transparent' />}
             </div>
             <div className='flex gap-2'>
-              <Button onMouseUp={() => {setTypeQRCode('png');}}onClick={downloadPng} icon={<DownloadOutlined />}>PNG</Button>
-              <Button onMouseUp={() => {setTypeQRCode('svg');}}className="bg-black font-bold" type='primary' onClick={downloadSvg} icon={<DownloadOutlined />}>SVG</Button>
+              <Button onMouseUp={() => { setTypeQRCode('png'); }} onClick={downloadPng} icon={<DownloadOutlined />}>PNG</Button>
+              <Button onMouseUp={() => { setTypeQRCode('svg'); }} className="bg-black font-bold" type='primary' onClick={downloadSvg} icon={<DownloadOutlined />}>SVG</Button>
             </div>
           </div>
         </div>
